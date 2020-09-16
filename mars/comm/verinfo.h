9c7d5d0f706dc7cd0635199fe06a2d45cd967689
Zhengji Lu <zhengji.lu@tes-tec.com>
2020-09-16 16:24:01 +0800

    解决Android编译错误的问题.

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "c58074b3"
+#define MARS_REVISION "78d0a0ab"
 #define MARS_PATH "master"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2020-07-24 15:11:06"
+#define MARS_BUILD_TIME "2020-09-16 16:22:09"
 #define MARS_TAG ""
 
 #endif
